09c4c1370788bc761ab4b034525009aca1f94984
Douglas DUTEIL <douglasduteil@gmail.com>
2021-04-30 14:04:01 +0200

    ci(github): test Dhall freezer (6)

--- actions/dhall-freezer/entrypoint.sh
+++ actions/dhall-freezer/entrypoint.sh
@@ -7,5 +7,3 @@ set -euo pipefail
 find . -type f -name '*.dhall' | sort -u | while read -r fpath; do
   echo "${fpath}" | xargs -t dhall freeze --inplace
 done
-
-git diff --color=always --exit-code
